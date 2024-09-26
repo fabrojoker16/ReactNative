92890b43be967565c7f92a0cc90bc3e8f21c52c9
fabrojoker16 <fabromusic-27@hotmail.com>
2024-09-25 21:01:49 -0500

    Se agrega el proyecto

---
+++ App.js
@@ -0,0 +1,48 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Main } from "./components/main";
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <StatusBar style="light" />
+        <Main />
+      </View>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 10,
+    color: "#fff",
+  },
+  description: {
+    fontSize: 16,
+    color: "#eee",
+  },
+  score: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "green",
+    marginBottom: 10,
+  },
+  card: {
+    marginBottom: 42,
+  },
+  image: {
+    width: 107,
+    height: 147,
+    borderRadius: 10,
+  },
+});
---
+++ components/main.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, ScrollView, Image, View } from "react-native";
+import { getLatestGames } from "../lib/metacritic";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export function Main() {
+  const [games, setGames] = useState([]);
+  const insets = useSafeAreaInsets();
+  useEffect(() => {
+    getLatestGames().then((games) => {
+      setGames(games);
+    });
+  }, []);
+  return (
+    <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
+      <ScrollView>
+        {games.map((game) => (
+          <View key={game.slug} style={styles.card}>
+            <Image source={{ uri: game.image }} style={styles.image} />
+            <Text style={styles.score}>{game.score}</Text>
+            <Text style={styles.title}>{game.title}</Text>
+            <Text style={styles.description}>{game.description}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 10,
+    color: "#fff",
+  },
+  description: {
+    fontSize: 16,
+    color: "#eee",
+    marginTop: 10,
+  },
+  score: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "green",
+    marginBottom: 10,
+  },
+  card: {
+    marginBottom: 50,
+    padding: 10,
+  },
+  image: {
+    width: 107,
+    height: 147,
+    borderRadius: 10,
+  },
+});
